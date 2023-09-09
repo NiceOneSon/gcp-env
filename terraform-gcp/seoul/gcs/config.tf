2313b6995fba64a49711a9d6f59c77cc63e68154
riiid <riiid@mb-jinyoungson.local>
2023-09-09 20:33:37 +0900

    feat:resolve

--- terraform-gcp/seoul/gcs/config.tf
+++ terraform-gcp/seoul/gcs/config.tf
@@ -4,13 +4,13 @@ terraform {
   backend "gcs" {
     bucket = "gcp-service-dev-terraform-1-1-0"
     key    = "seoul/tfstate"
-    
+
   }
 }
 
 provider "google" {
   project = local.tags.project
-  region  = module.const.dev.project.
+  region  = module.const.dev.project.region
 }
 
 module "const" { source = "./../const" }
