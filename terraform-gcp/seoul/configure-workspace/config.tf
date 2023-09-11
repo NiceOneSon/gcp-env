9ded552b68e777fa79497a8f9386eccb9472f36e
JinyoungSon <rmflwhdhk1@gmail.com>
2023-09-11 23:17:43 +0900

    feat:added vpc

--- terraform-gcp/seoul/configure-workspace/config.tf
+++ terraform-gcp/seoul/configure-workspace/config.tf
@@ -11,6 +11,7 @@ terrafrom {
 provider "google" {
   project = "terraform-on-gcp-39710"
   region  = module.const.regions.seoul
+  credentials = file("credential/terraform-on-gcp-39710-7b16e8345a6b.json")
 }
 
 module "const" { source = "./../const" }
